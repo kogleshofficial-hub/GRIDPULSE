78236234ba167dddd232f41b22ec89ad8d637ed3
Koglesh R. Murugan <koglesh.official@gmail.com>
2026-09-05 10:16:35 +0800

    fix: resolve ESLint Next config ESM import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import nextVitals from "eslint-config-next/core-web-vitals.js";
 
 export default defineConfig([
   ...nextVitals,
-  globalIgnores([".next/**","node_modules/**"])
+  globalIgnores([".next/**", "node_modules/**"]),
 ]);
